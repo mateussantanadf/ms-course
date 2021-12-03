cc605b1625bbea6de78adeb4ed5b426902c18464
mateussantanadf <santanamateus02@gmail.com>
2021-12-03 11:15:36 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/mateus/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
